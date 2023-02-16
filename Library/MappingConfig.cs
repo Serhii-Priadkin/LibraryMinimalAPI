334a3529ff7b80b86e05f5381be7367a0b87d7fc
Serhii Priadkin <sergio.pryadkin@gmail.com>
2023-02-16 16:40:09 +0200

    Added AutoMapper

---
+++ Library/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Library.Models;
+using Library.Models.DTO;
+
+namespace Library
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Book,BookCreateDTO>().ReverseMap();
+        }
+    }
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using Library;
 using Library.Data;
 using Library.Models;
 using Library.Models.DTO;
@@ -10,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,33 +26,25 @@ app.MapGet("/api/books", (ILogger<Program> _logger) =>
 {
     _logger.Log(LogLevel.Information, "Getting all Books");
     return Results.Ok(BookStore.bookList);
-});
+}).Produces<IEnumerable<Book>>(200);
 
 app.MapGet("/api/books/{id:int}",(int id) =>
 {
     return Results.Ok(BookStore.bookList.FirstOrDefault(u => u.Id == id));
-});
+}).Produces<Book>(200);
 
-app.MapPost("/api/books", ([FromBody] BookCreateDTO book_C_DTO) =>
+app.MapPost("/api/books", (IMapper _mapper, [FromBody] BookCreateDTO book_C_DTO) =>
 {
     if (book_C_DTO.Id == 0|| book_C_DTO.Id > BookStore.bookList.OrderByDescending(u => u.Id).FirstOrDefault().Id)
     {
         book_C_DTO.Id = BookStore.bookList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
     }
 
-    Book book = new()
-    {
-        Id = book_C_DTO.Id,
-        Title = book_C_DTO.Title,
-        Cover = book_C_DTO.Cover,
-        Content = book_C_DTO.Content,
-        Genre = book_C_DTO.Genre,
-        Author = book_C_DTO.Author
-    };
+    Book book = _mapper.Map<Book>(book_C_DTO);
 
     BookStore.bookList.Add(book);
-    return Results.Ok(book);
-});
+    return Results.Ok(book.Id);
+}).Produces<Book>(201);
 
 
 
