35540bdf937d12dc7565f1a51816e03e17ba68c6
Serhii Priadkin <sergio.pryadkin@gmail.com>
2023-02-17 17:27:33 +0200

    Fixed bugs in Get method for ordering by provided value(title or author)

--- Library/Program.cs
+++ Library/Program.cs
@@ -23,13 +23,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/api/books", (ILogger<Program> _logger) =>
+app.MapGet("/api/books{title_or_author}", (ILogger<Program> _logger,string title_or_author) =>
 {
-    _logger.Log(LogLevel.Information, "Getting all Books");
+    _logger.Log(LogLevel.Information, $"Ordering all Books by {title_or_author}");
     var res = from b in BookStore.bookList
               join rat in RatingStore.ratingList on b.Id equals rat.BookId into rati
               join rev in ReviewStore.reviewList on b.Id equals rev.BookId into revi
-
+              orderby (title_or_author == "title"|| title_or_author == "Title"?b.Title:b.Author)
               select new
               {
                   b.Id,
@@ -38,13 +38,7 @@ app.MapGet("/api/books", (ILogger<Program> _logger) =>
                   b.Cover,
                   b.Content,
                   rating = rati.Average(s => s.Score),
-                  reviews = from r in revi
-                            select new
-                            {
-                                r.Id,
-                                r.Message,
-                                r.Reviewer
-                            }
+                  reviews = revi.Count()
               };
     return Results.Ok(res);
 }).Produces<IEnumerable<Book>>(200);
@@ -76,7 +70,7 @@ app.MapGet("/api/books/{id:int}", (ILogger<Program> _logger, int id) =>
     return Results.Ok(res.FirstOrDefault(u => u.Id == id));
 }).Produces<Book>(200);
 
-app.MapGet("/api/{genre?=Horror}", (ILogger<Program> _logger) =>
+app.MapGet("/api/horror", (ILogger<Program> _logger) =>
 {
     _logger.Log(LogLevel.Information, "Getting all Books");
     var res = from b in BookStore.bookList where b.Genre=="Horror"
